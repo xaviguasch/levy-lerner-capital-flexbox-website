ebd87b5942898f8bd6a979bffb3198fa2787aebb
xaviguasch <xaviguasch@gmail.com>
2019-02-13 13:41:47 +0100

    making the navbar transparent when scrolling

--- main.js
+++ main.js
@@ -18,6 +18,15 @@ function initMap() {
     });
 }
 
+// Sticky menu background
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
 //Smooth Scrolling
 $('#navbar a, .btn').on('click', function (event) {
     if (this.hash !== '') {
