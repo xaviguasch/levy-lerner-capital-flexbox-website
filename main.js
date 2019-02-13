9230c9a04e04b01a42008b476c45f2e3e424cf84
xaviguasch <xaviguasch@gmail.com>
2019-02-13 13:39:23 +0100

    adding JQuery for smooth scrolling

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ function initMap() {
     const loc = {
         lat: 41.385063,
         lng: 2.173404
-    }
+    };
 
     // Centered map on location
     const map = new google.maps.Map(document.querySelector('.map'), {
         zoom: 14,
         center: loc
-    })
+    });
     // The marker, positioned at location
     const marker = new google.maps.Marker({
         position: loc,
         map: map
-    })
+    });
 }
+
+//Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+                scrollTop: $(hash).offset().top - 65
+            },
+            800
+        );
+    }
+});
